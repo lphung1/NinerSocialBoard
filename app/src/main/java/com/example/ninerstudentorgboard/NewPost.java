16b0bf033f6af7e6e4df5b14eb25bed9dfbe69a6
jmoua4 <jmoua4@uncc.edu>
2019-04-25 16:03:27 -0400

    added uploadImage function to New Post, not stored in Post yet

--- app/src/main/java/com/example/ninerstudentorgboard/NewPost.java
+++ app/src/main/java/com/example/ninerstudentorgboard/NewPost.java
@@ -1,8 +1,13 @@
 package com.example.ninerstudentorgboard;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.ninerstudentorgboard.PostListFragment;
 import com.example.ninerstudentorgboard.MainActivity;
@@ -28,6 +34,10 @@ public class NewPost extends AppCompatActivity {
     private EditText eventTitleEditText;
     private TextView tagEditText;
     private boolean dateSet = false;
+    //Jerry Image
+    private ImageView imageUpload;
+    private Button imageUploadButton;
+    private static final int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +49,10 @@ public class NewPost extends AppCompatActivity {
         tagEditText = findViewById(R.id.tagEditText_newPost);
         eventTitleEditText = findViewById(R.id.postTitleEditText_newPost);
         final EditText postContent = findViewById(R.id.postEditText_newPost);
+
+        //Image Upload
+        imageUpload = findViewById(R.id.imageViewUploadPost);
+        imageUploadButton = findViewById(R.id.UploadImage_button_newpost);
 
         //submits entreis when submit button is pressed
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -96,7 +110,28 @@ public class NewPost extends AppCompatActivity {
         };
 
 
+        //Image Upload
+        imageUploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
 
+            }
+
+
+        } );
+
+    }
+
+    //Needed for Image upload
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            imageUpload.setImageURI(selectedImage);
+        }
     }
 }
 
